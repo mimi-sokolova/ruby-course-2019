3229273eab046c9c61ae9a33c2110287b99589e7
milenasokolova <milena.sokolova@sumup.com>
2019-02-18 17:37:35 +0200

    hw1

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,18 +1,20 @@
 # Method returns the constant 4 for any input parameter.
 def f_c(x)
-  return 4
+  $c = 4
+  return $c
 end
 
 # Method calculate the formula f(x) = a*x + b
 def f_x(x, a, b)
-f = x*a+b
+f = (x*a)+b
+  return f
 end
 
 # Method calculates the sum of f(x) with a and b from 1 to 3
 def sum(x)
   sum = 0
   for i in 1..3 do
-sum += f_c(x, i,i)
+sum = sum + self.f_c(x, i,i)
   end
   return sum
 end
