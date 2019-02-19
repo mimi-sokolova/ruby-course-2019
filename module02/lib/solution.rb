2b70dfc0130a23ca6804ac9f1a6c30dbfc3b0b1b
milenasokolova <milena.sokolova@sumup.com>
2019-02-19 18:36:17 +0200

    added the constants and the methods

--- module02/lib/solution.rb
+++ module02/lib/solution.rb
@@ -1,3 +1,8 @@
+IS_TRUE = true
+IS_FALSE = false
+PANCAKE_INGREDIENTS = {flour => 2, eggs => 4, milk => 200, butter => false, salt => 0.001}
+FIBONACCI_NUMBERS = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+
 def num_add(a, b)
   a + b
 end
@@ -18,5 +23,25 @@ def num_div (а,b)
 end
 
 def num_floor(a, b)
+
+end
+
+def ingredient_exists(ingr, hash)
+
+end
+
+def fatten_pancakes(hash)
+
+end
+
+def remove_salt(hash)
+
+end
+
+def add_fibonacci(arr)
+
+end
+
+def fib_exists(arr, n)
 
 end
