b6aafd01046e8209ecd1ba7df1a9f9b594218a10
milenasokolova <milena.sokolova@sumup.com>
2019-02-19 10:29:03 +0200

    updeted the hw with the comments

--- lib/solution.rb
+++ lib/solution.rb
@@ -5,15 +5,10 @@ end
 
 # Method calculate the formula f(x) = a*x + b
 public def f_x(x, a, b)
-f = (x*a)+b
-  return f
+  (x*a)+b
 end
 
 # Method calculates the sum of f(x) with a and b from 1 to 3
 def sum(x)
-  sum = 0
-  for i in 1..3 do
-sum = sum + f_x(x, i,i)
-  end
-  return sum
+ f_x(x, 1,1)+f_x(x, 2,2)+f_x(x, 3,3)
 end
