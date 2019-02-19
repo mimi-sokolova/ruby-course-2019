629ac42be7131da69f501789d9b16e58cba368e6
milenasokolova <milena.sokolova@sumup.com>
2019-02-19 15:57:41 +0200

    adding the first methods

---
+++ module02/lib/solution.rb
@@ -0,0 +1,22 @@
+def num_add(a, b)
+  a + b
+end
+
+def num_sub(a, b)
+  a - b
+end
+
+def num_mul(a, b)
+  a * b
+end
+
+def num_div (а,b)
+  if b==0
+    puts "b is zero! You can not dived by zero!"
+    else  a/b
+  end
+end
+
+def num_floor(a, b)
+
+end
