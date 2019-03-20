41fd49a467666fc761ec73d417b403cc9cf98a29
milenasokolova <milena.sokolova@sumup.com>
2019-03-20 13:07:03 +0200

    -

---
+++ module04/02-Bank-Account/lib/solution-backaccount.rb
@@ -0,0 +1,71 @@
+class BankAccount
+
+  def initialize(name, balance, currency)
+
+    if balance < 0
+      raise ArgumentError.new("Account balance can't be negative")
+    end
+    if currency == ""
+      raise ArgumentError.new("You can not have a account without currency")
+    end
+    @name = name
+    @balance = balance
+    @currency = currency
+    @history = ["Account was created"]
+  end
+
+  def currency
+    @currency
+  end
+
+  def deposit(amount)
+
+    if amount < 0
+      raise ArgumentError.new("You can not deposit a negative number")
+    end
+    @history.append("Deposited #{amount}#{@currency}")
+    @balance += amount
+  end
+
+  def balance
+    @history.append("Balance check -> #{@balance}#{@currency}")
+    @balance
+  end
+
+  def withdraw(amount)
+
+    if amount > @balance
+      @history.append("Withdraw for #{amount}#{@currency} failed.")
+      return false
+    else
+      @balance -= amount
+      @history.append("#{amount}#{@currency} was withdrawn")
+      return true
+    end
+
+  end
+
+  def to_s()
+    "Bank account for #{@name} with balance of #{@balance}#{@currency}"
+  end
+
+  def history
+    @history
+  end
+
+  def transfer_to(account, amount)
+    if account.currency != @currency
+      return false
+    end
+    if @balance < amount
+      return false
+    end
+
+    self.withdraw(amount)
+    account.deposit(amount)
+    return true
+
+  end
+
+end
+
